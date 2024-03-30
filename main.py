f4c69ddff3add5d22e74641c396f4d7654e2fd16
Sakthi Prakash K <92044150+SAKTHIPRAKASH28@users.noreply.github.com>
2024-03-30 17:56:04 +0530

    exceptions handled

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import (FastAPI, status, Depends)
+from fastapi.responses import JSONResponse
 from src.utils import *
 from src.emailotp import *
 
@@ -8,28 +9,37 @@ app = FastAPI()
 @app.post("/addUser", status_code=status.HTTP_201_CREATED)
 async def new_user(user: NewUser,otp:str=None):
     if not otp:
-        return send_email(user.email)
+        try:
+            return JSONResponse(content=send_email(user.email),status_code=201)
+        except HTTPException as e:
+            return JSONResponse(content={"error": str(e)}, status_code=e.status_code)
     else:
-        if validate_otp(user.email,otp):
+        try :
+            validate_otp(user.email,otp)
             result = await add_user(user.username, user.password,user.email)
-            return result
-        else:
-            raise HTTPException(status_code=401, detail="Invalid OTP")
+            return JSONResponse(content=result,status_code=201)
+        except HTTPException as e:
+            return JSONResponse(content={"error": str(e)}, status_code=e.status_code)
 
 
 @app.post("/getJwt", status_code=status.HTTP_201_CREATED)
 async def generate_JWT(user: UserModel):
-    result, _id = await verify_password(user.username, user.password)
-    if result:
-        token = await generate_tokens(600, _id)
-        return token
-    else:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-
+    try:
+        result, _id = await verify_password(user.username, user.password)
+        if result:
+            token = await generate_tokens(600, _id)
+            return JSONResponse(content=token,status_code=201)
+        else:
+            return JSONResponse(status_code=401, content="Invalid credentials")
+    except HTTPException as e:
+            return JSONResponse(content={"error": str(e)}, status_code=e.status_code)
 
 @app.post("/refresh", status_code=status.HTTP_201_CREATED)
 async def refresh(token: str):
-    return await refresh_token(token)
+    try:
+        await refresh_token(token)
+    except HTTPException as e:
+            return JSONResponse(content={"error": str(e)}, status_code=e.status_code)
 
 
 if __name__ == '__main__':
